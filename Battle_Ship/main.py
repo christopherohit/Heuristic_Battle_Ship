fd83807b48aa15e689cfa87f64a3747c4ecd2240
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-10-10 05:31:31 +0300

    Run main for game and try to fix bug in here

---
+++ Battle_Ship/main.py
@@ -0,0 +1,15 @@
+# from intro import Intro
+# from _config import Creat_Background
+
+hang , cot = 2 , 1
+c = cot
+a = [[1,613,5,32,32,4613,13,13,1],[9,2,6,4,7,8,5,6,100],[45,1,3,95,45,12,66,12,32],[4,56,13,12,784,46,11,32,56]]
+b = []
+
+while cot < c + 5:
+    a[hang][cot] = "X"
+    cot += 1
+        
+print(a)
+# if __name__ == "__main__":
+#     Intro()
