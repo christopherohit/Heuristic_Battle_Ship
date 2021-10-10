78c12979c5da99bc23ee87211f0e430e49e03d59
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-10-10 05:30:51 +0300

    Presentation and Introdution Game

---
+++ Battle_Ship/intro.py
@@ -0,0 +1,25 @@
+from _config import *
+import time
+
+def Intro():
+    print("Trò chơi bắn tàu chiến giữa biển khơi mênh mông đối diện chúng ta là một kẻ thù khá là mạnh")
+    time.sleep(0.5)
+    print("Hãy vận dụng khả năng tiên đoán của Edogawa Conan bên trong bạn để tiêu diệt toàn bộ tàu chiến")
+    time.sleep(0.5)
+    print("của đối phương đem lại hòa bình cho nước nhà nhưng thật kì lạ bộ tổng tư lệnh chỉ cho bạn được")
+    time.sleep(0.5)
+    print("sử dụng đúng 5 tàu chiến gồm 1 tàu che chắn to 5 đơn vị , 1 tàu hậu phương to 4 đơn vị , 2 tàu")
+    time.sleep(0.5)
+    print("Ngư lôi to 3 đơn vị và 1 tàu đại bác to 2 đơn vị. Bạn chỉ có 5 lượt bắn tướng ứng với số tàu bạn")
+    time.sleep(0.5)
+    print("sỡ hữu bạn có thể thấy rằng tàu 3 và tàu 2 khá quan trọng vì nó là có thể tấn công được vậy nên khi mất")
+    time.sleep(0.5)
+    print("nó đối phương sẽ được một số điểm khá lớn đấy vậy nên hãy vận dụng sự thông thái và khôn ngoan trong")
+    time.sleep(0.5)
+    print("việc sắp xếp các tàu và sự Pro của Cô Văn Nan trong việc đoán ra vị trị tàu của đoán phương nhé")
+    time.sleep(0.5)
+    print("Chúc các bạn chơi vui vẻ !!!")
+    time.sleep(1)
+
+    print("Hãy tạo ra kích thước cho vùng biển của bạn đi nào")
+    Creat_Background()
