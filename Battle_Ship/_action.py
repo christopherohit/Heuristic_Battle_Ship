d8f61695e8eaa21ba263201233b0bfb37d099aa6
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-10-10 11:17:10 +0300

    Transfer to English

--- Battle_Ship/_action.py
+++ Battle_Ship/_action.py
@@ -2,21 +2,21 @@
 
 # Trường nhập tọa độ để bắn phá tàu đối phương
 def Fight_ship_player(m,n):
-    cot = int(input("Hãy Nhập Vị Trí Để Bắn Tàu (từ 0 đến {:d} theo hướng thẳng):".format(m-1)))
+    cot = int(input("Please Enter Location To Shoot Ships (from 0 to {:d} in the straight direction):".format(m-1)))
     while cot not in range(m):
         rule = True
         while rule:
-            print("Bạn bắn tàu ngoài phạm vi khu vực chơi!")
-            cot = input("Hãy Nhập Vị Trí Để Bắn Tàu(từ 0 đến {:d}):".format(m -1))
+            print("You shoot ships outside the play area!")
+            cot = input("Please Enter Location To Shoot Ships (from 0 to {:d}):".format(m -1))
             if cot in range(n):
                 rule = False
                 break    
-    hang = int(input("Hãy nhập vị trí để bắn tàu (Từ 0 - {:d} theo hướng nằm ngang):".format(n-1)))
+    hang = int(input("Please Enter Location To Shoot Ships (from 0 to {:d} in the vertical direction):".format(n-1)))
     while hang not in range(n):
         rule = True
         while rule:
-            print("Bạn Bắn ra ngoài phạm vi khu vực chơi!")
-            hang = input("Hãy Nhập Vị Trí Để bắn Tàu(từ 0 đến {:d}):".format(n - 1))
+            print("You shoot ships outside the play area!")
+            hang = input("Please Enter Location To Shoot Ships (from 0 to {:d}):".format(n - 1))
             if hang in range(n):
                 rule = False
                 break
@@ -26,22 +26,22 @@ def Fight_ship_player(m,n):
 
 
 # Nhập Tọa độ để đặt tàu
-def cung_cap_vi_tri_de_ban(m,n , i):
-    cot = input("Hãy Nhập Vị Trí Để Đặt Tàu {:d} (từ 0 đến {:d} theo hướng thẳng):".format(i,m-1))
+def Approve_Location_To_Set_Ship(m,n , i):
+    cot = input("Please Enter Location To Set Ship {:d} (from 0 to {:d} in the horizontal directions):".format(i,m-1))
     while cot not in range(m):
         rule = True
         while rule:
-            print("Bạn bắn tàu ngoài phạm vi khu vực chơi!")
-            cot = input("Hãy Nhập Vị Trí Để Đặt Tàu(từ 0 đến {:d}):".format(m-1))
+            print("You was set ships outside the play area!")
+            cot = input("Please Enter Location To Set Ship (from 0 to {:d}):".format(m-1))
             if cot in range(n):
                 rule = False
                 break 
-    hang = input(" Hãy nhập vị trí đặt tàu {:d} (Từ 0 - {:d} theo hướng nằm ngang):".format(i, n - 1))
+    hang = input("Please Enter Location To Set Ship {:d} (from 0 to {:d} in the vertical directions):".format(i, n - 1))
     while hang not in range(n):
         rule = True
         while rule:
-            print("Bạn Đặt ra ngoài phạm vi khu vực chơi!")
-            hang = input("Hãy Nhập Vị Trí Để đặt Tàu(từ 0 đến {:d}):".format(n - 1))
+            print("You was set ships outside the play area!")
+            hang = input("Please Enter Location To Set Ship (from 0 to {:d}):".format(n - 1))
             if hang in range(n):
                 rule = False
                 break
@@ -50,4 +50,3 @@ def cung_cap_vi_tri_de_ban(m,n , i):
 
 
 
-Fight_ship_player(9 , 9)
