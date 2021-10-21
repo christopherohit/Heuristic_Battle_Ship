00ba337d81cc223420d68d91bc31f849f55cf280
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-10-21 22:16:53 +0300

    Check and Select case For issues

---
+++ Ocean/Distance.py
@@ -0,0 +1,11 @@
+from point_in_map import Check_Easy_Situation
+from Compare import No_Solution
+
+
+def Except():
+    if Check_Easy_Situation == 0 or No_Solution is True:
+        print("Ya, You are stand on special situation, which very very hard solve")
+        print('And I need a less time more to find out solution to solve it')
+        print("I'm really sorry for this non-convenience, Thanks use it")
+    else:
+        pass
