9a837f8ba26a3bbb2fafdb6ffbe83ba8a39e5ba2
christopherohit <55018999+christopherohit@users.noreply.github.com>
2021-10-31 11:51:25 +0300

    Test Logic code

--- Ocean/Try_Logic_Test.py
+++ Ocean/Try_Logic_Test.py
@@ -1,10 +1,26 @@
-i = list(map(int,input().split()))
-a = i[:-1]
-s = i[-1]
-cout = 0
-for j in range(len(a)):
-    if a[j] == s:
-        print(j + 1,end=" ")
-        cout += 1
-if cout == 0:
-    print(-1)
+import math
+
+n = int(input())
+a = list(map(int , input().split()))
+max1 = 0
+
+def sss(i, max1):
+    if i <= max1:
+        max1 = max1
+    else:
+        max1 = i
+    return max1
+
+for i in range(len(a)):
+    try:
+        if a[i] < 0 or a[i+1] < 0:
+            continue
+        else:
+            k = str(a[i]) + str(a[i+1])
+            max1 = sss(int(k),max1)
+    except:
+        k = str(a[n-1]) + str(a[0])    
+        max1 = sss(int(k), max1)
+max1 = str(max1)
+for i in max1:
+    print(i , end=" ")
